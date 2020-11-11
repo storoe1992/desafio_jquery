b02ad8d6e6a89fd46ef527ff992ba099772ed79f
Sergio Toro Escofet <storoe1992@gmail.com>
2020-11-11 10:07:10 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('a').attr('target','blank');
+    $('a').prepend(function(index,html){
+        switch (html){
+            case 'Facebook' : $(this).prepend('<i class="fab fa-facebook-f"></i>');break;
+            case 'Instagram' : $(this).prepend('<i class="fab fa-instagram"></i>');break;
+            case 'Twitter' : $(this).prepend('<i class="fab fa-twitter"></i>');break;
+            case 'Snapchat' : $(this).prepend('<i class="fab fa-pinterest"></i>');break;
+        }
+    });
+   $("h2").click(function(){
+       $(this).css("color","red");
+   });
+   $("section:nth-child(3) li:nth-child(4)")
+   .append('<ul><li><a href="#">link 4.a</a></li></ul>')
+   .after('<li><a href="#">link 5</a></li>');
+   ;
+});
